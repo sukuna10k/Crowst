aa4631f7b0825239cd5a3c41fdda4dc4329ca500
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-01-05 13:32:23 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,68 +1,67 @@
-#(©)t.me/CodeFlix_Bots
-
-
-
-
 import os
-from os import environ,getenv
 import logging
 from logging.handlers import RotatingFileHandler
+from dotenv import load_dotenv
 
+load_dotenv()
 
+# Jeton du bot @Botfather
+TG_BOT_TOKEN = os.environ.get("7783135016:AAHjQYyKamQ-PPPIK6vr9au82vtPln-o5V4")
 
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-#Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
-#Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
-#Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001995978690"))
-# NAMA OWNER
-OWNER = os.environ.get("OWNER", "sewxiy")
-#OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "6497757690"))
-#Port
-PORT = os.environ.get("PORT", "8030")
-#Database
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+# Votre API ID de my.telegram.org
+APP_ID = int(os.environ.get("24817837"))
 
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001868871195')) #Log channel id ( make sure bot is admin )
+# Votre API Hash de my.telegram.org
+API_HASH = os.environ.get("acd9f0cc6beb08ce59383cf250052686")
 
-#force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001473043276"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001644866777"))
+# ID de votre canal DB
+CHANNEL_ID = int(os.environ.get("-1002491166640"))
 
+# ID du propriétaire
+OWNER_ID = int(os.environ.get("7428552084"))
+
+# Port
+PORT = os.environ.get("PORT", "8080")
+
+# Base de données
+DB_URI = os.environ.get("mongodb+srv://Aniflix:Lipun123@aniflix.q2wina5.mongodb.net/?retryWrites=true&w=majority&appName=Aniflix")
+DB_NAME = os.environ.get("Antiflix")
+
+# ID du canal de souscription forcée, si vous voulez activer la souscription forcée
+FORCE_SUB_CHANNEL_1 = int(os.environ.get("-1002172537293"))
+FORCE_SUB_CHANNEL_2 = int(os.environ.get("-1002327419520"))
+FORCE_SUB_CHANNEL_3 = int(os.environ.get("-1002278892364"))
+FORCE_SUB_CHANNEL_4 = int(os.environ.get("-1002309325441"))
+
+# Nombre de travailleurs du bot TG
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3312d6a.jpg")
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
-
-HELP_TXT = "<b>ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋ ғᴏʀ @otakuflix_network\n\n❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n├/start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n├/about : ᴏᴜʀ Iɴғᴏʀᴍᴀᴛɪᴏɴ\n└/help : ʜᴇʟᴘ ʀᴇʟᴀᴛᴇᴅ ʙᴏᴛ\n\n sɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴏɴ ʟɪɴᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ᴊᴏɪɴ ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴛʜᴀᴛs ɪᴛ.....!\n\n ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ <a href=https://t.me/cosmic_freak>sᴜʙᴀʀᴜ</a></b>"
-ABOUT_TXT = "<b>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/cosmic_freak>subaru</a>\n◈ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/otakuflix_network>ᴏᴛᴀᴋᴜғʟɪx ɴᴇᴛᴡᴏʀᴋ</a>\n◈ ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/anime_cruise_netflix>ᴀɴɪᴍᴇ ᴄʀᴜɪsᴇ</a>\n◈ sᴇʀɪᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/webseries_flix>ᴡᴇʙsᴇʀɪᴇs ғʟɪx</a>\n◈ ᴀᴅᴜʟᴛ ᴍᴀɴʜᴡᴀ : <a href=https://t.me/pornhwaocean>ᴘᴏʀɴʜᴡᴀs</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/cosmic_freak>subaru</a></b>"
-START_MSG = os.environ.get("START_MESSAGE", "<b>ʙᴀᴋᴋᴀᴀᴀ!! {first}\n\n ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</b>")
+# Message de démarrage
+START_MSG = os.environ.get("START_MESSAGE", "<b>ʙᴏɴᴊᴏᴜʀ {first}\n\n ᴊᴇ ꜱᴜɪꜱ ᴜɴ ʙᴏᴛ ᴅᴇ ꜱᴛᴏᴄᴋᴀɢᴇ ᴅᴇ ꜰɪᴄʜɪᴇʀꜱ ᴍᴜʟᴛɪᴘʟᴇ, ᴊᴇ ᴘᴇᴜx ꜱᴛᴏᴄᴋᴇʀ ᴅᴇꜱ ꜰɪᴄʜɪᴇʀꜱ ᴘʀɪᴠéꜱ ᴅᴀɴꜱ ᴜɴ ᴄᴀɴᴀʟ ꜱᴘéᴄɪꜰɪé ᴇᴛ ᴅ'ᴀᴜᴛʀᴇꜱ ᴜᴛɪʟɪꜱᴀᴛᴇᴜʀꜱ ᴘᴇᴜᴠᴇɴᴛ ʏ ᴀᴄᴄéᴅᴇʀ ᴠɪᴀ ᴜɴ ʟɪᴇɴ ꜱᴘéᴄɪᴀʟ » @AntiFlix_actu</b>")
 try:
-    ADMINS=[6376328008]
-    for x in (os.environ.get("ADMINS", "5115691197 6273945163 6103092779 5231212075").split()):
+    ADMINS = []
+    for x in (os.environ.get("ADMINS")).split():
         ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+    raise Exception("ᴠᴏᴛʀᴇ ʟɪꜱᴛᴇ ᴅ'ᴀᴅᴍɪɴɪꜱᴛʀᴀᴛᴇᴜʀꜱ ɴᴇ ᴄᴏɴᴛɪᴇɴᴛ ᴘᴀꜱ ᴅ'ᴇɴᴛɪᴇʀꜱ ᴠᴀʟɪᴅᴇꜱ.")
 
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
+# Message de souscription forcée 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ᴅéꜱᴏʟé {first}, ᴛᴜ ᴅᴏɪꜱ ᴅ'ᴀʙᴏʀᴅ ʀᴇᴊᴏɪɴᴅʀᴇ ᴍᴇꜱ ᴄʜᴀîɴᴇꜱ ᴘᴏᴜʀ ᴀᴄᴄéᴅᴇʀ ᴀᴜx ꜰɪᴄʜɪᴇʀꜱ..\n\n ᴀʟᴏʀꜱ, ꜱ'ɪʟ ᴛᴇ ᴘʟᴀîᴛ, ʀᴇᴊᴏɪɴꜱ ᴍᴇꜱ ᴄʜᴀîɴᴇꜱ ᴅ'ᴀʙᴏʀᴅ ᴇᴛ ᴄʟɪQᴜᴇ ꜱᴜʀ ʟᴇ ʙᴏᴜᴛᴏɴ 'ᴄʟɪQᴜᴇᴢ ɪᴄɪ ᴍᴀɪɴᴛᴇɴᴀɴᴛ'....!")
 
-#set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @OtakuFlix_Network</b>")
+# Définissez votre légende personnalisée ici, mettez None pour désactiver la légende personnalisée
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "None")
 
-#set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+# Définir True si vous voulez empêcher les utilisateurs de transférer des fichiers du bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
-#Set true if you want Disable your Channel Posts Share button
+# Définir True si vous voulez désactiver le bouton de partage des publications de votre canal
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
+# Texte des statistiques du bot
+BOT_STATS_TEXT = "<b>UPTIME DU BOT</b>\n{uptime}"
+
+# Texte de réponse utilisateur
+USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ᴛᴜ ɴ'ᴇs ᴘᴀs ᴍᴏɴ ꜱᴇɴᴘᴀɪ!!\n\n» ᴍᴏɴ ᴘʀᴏᴘʀɪéᴛᴀɪʀᴇ : @Kingcey"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6497757690)
@@ -87,4 +86,3 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
-   
